f4e7c047c04ab0c976ea9c0036f076d0ac465f81
SimoneCerri <simone.cerri1994@gmail.com>
2024-04-16 15:53:49 +0200

    init + structure with bootstrap

---
+++ index.php
@@ -0,0 +1,236 @@
+<?php
+
+
+
+
+
+?>
+
+
+
+
+
+<!doctype html>
+<html lang='en'>
+
+<head>
+
+    <title>CHANGE TITLE</title>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <!-- #region link to Style -->
+    <link href='./assets/css/style.css' rel='stylesheet'>
+    <!-- #endregion link to Style -->
+    <!-- #region link to FontAwesome (icons) -->
+    <link rel='stylesheet' href='https: //cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.    css' integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIft+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==' crossorigin='anonymous' referrerpolicy='no-referrer' />
+    <!-- #endregion link to FontAwesome (icons) -->
+    <!-- #region links to GoogleFonts (Nanum Gothic Coding) -->
+    <link rel='preconnect' href='https: //fonts.googleapis.com'>
+    <link rel='preconnect' href='https: //fonts.gstatic.com' crossorigin>
+    <link href='https: //fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&display=swap' rel='stylesheet'>
+    <!-- #endregion links to GoogleFonts (Nanum Gothic Coding) -->
+    <!-- #region link to Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- #endregion link to Bootstrap -->
+
+</head>
+
+<body class='debug'>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="/">Newsletter</a>
+                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavId">
+                    <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="/" aria-current="page">Home
+                                <span class="visually-hidden">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contacts</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+
+    <main>
+        <div class="p-5 mb-4 bg-light rounded-3">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">Welcome guest !</h1>
+                <p class="col-md-8 fs-4">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta fuga magni sequi cumque nemo aperiam ab adipisci, neque veniam culpa nostrum, in ad ullam? Dolorem!
+                </p>
+                <button class="btn btn-primary btn-lg" type="button">
+                    Search me
+                </button>
+            </div>
+        </div>
+
+        <section class="posts my-5">
+            <div class="container">
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4">
+                    <div class="col">
+                        <div class="card">
+                            <img class="card-img-top" src="https://picsum.photos/200/300" alt="Title" />
+                            <div class="card-body">
+                                <h4 class="card-title">Title</h4>
+                                <p class="card-text">Text</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img class="card-img-top" src="https://picsum.photos/200/300" alt="Title" />
+                            <div class="card-body">
+                                <h4 class="card-title">Title</h4>
+                                <p class="card-text">Text</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img class="card-img-top" src="https://picsum.photos/200/300" alt="Title" />
+                            <div class="card-body">
+                                <h4 class="card-title">Title</h4>
+                                <p class="card-text">Text</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img class="card-img-top" src="https://picsum.photos/200/300" alt="Title" />
+                            <div class="card-body">
+                                <h4 class="card-title">Title</h4>
+                                <p class="card-text">Text</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img class="card-img-top" src="https://picsum.photos/200/300" alt="Title" />
+                            <div class="card-body">
+                                <h4 class="card-title">Title</h4>
+                                <p class="card-text">Text</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img class="card-img-top" src="https://picsum.photos/200/300" alt="Title" />
+                            <div class="card-body">
+                                <h4 class="card-title">Title</h4>
+                                <p class="card-text">Text</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img class="card-img-top" src="https://picsum.photos/200/300" alt="Title" />
+                            <div class="card-body">
+                                <h4 class="card-title">Title</h4>
+                                <p class="card-text">Text</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img class="card-img-top" src="https://picsum.photos/200/300" alt="Title" />
+                            <div class="card-body">
+                                <h4 class="card-title">Title</h4>
+                                <p class="card-text">Text</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="newsletter bg-secondary text-white">
+            <div class="container">
+                <h3>
+                    Newsletter
+                </h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut excepturi quisquam illo ad, nihil fugit, architecto vitae et iste quas, autem hic laudantium ipsam numquam.
+                </p>
+
+                <form action="" method="get" class="d-flex justify-content-center align-items-center">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control rounded-0" name="email" id="email" aria-describedby="emailHelper" placeholder="" />
+                        <small id="emailHelper" class="form-text text-muted">Type your email here</small>
+                    </div>
+                    <button type="submit" class="btn btn-dark rounded-0">Submit</button>
+
+                </form>
+            </div>
+        </section>
+
+    </main>
+
+    <footer class="bg-dark py-4 text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h3>Company</h3>
+                    <ul class="list-unstyled">
+                        <li><a href="">Lorem.</a></li>
+                        <li><a href="">Accusantium?</a></li>
+                        <li><a href="">Animi!</a></li>
+                        <li><a href="">Quos?</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h3>Links</h3>
+                    <ul class="list-unstyled">
+                        <li><a href="">Lorem.</a></li>
+                        <li><a href="">Accusantium?</a></li>
+                        <li><a href="">Animi!</a></li>
+                        <li><a href="">Quos?</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h3>Legal</h3>
+                    <ul class="list-unstyled">
+                        <li><a href="">Lorem.</a></li>
+                        <li><a href="">Accusantium?</a></li>
+                        <li><a href="">Animi!</a></li>
+                        <li><a href="">Quos?</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- #region Dev-only cdns, disable in production -->
+    <!-- #region link to Vue-script -->
+    <!-- <script src='https: //unpkg.com/vue@3/dist/vue.global.js'></script> -->
+    <!-- #endregion link to Vue-script -->
+    <!-- #region link to Bootstrap-script -->
+    <!-- <script src='https: //cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js' integrity='sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz' crossorigin='anonymous'></script> -->
+    <!-- #endregion link to Bootstrap-script -->
+    <!-- #region link to Script -->
+    <!-- <script src='./assets/js/main.js'></script> -->
+    <!-- #endregion link to Script -->
+    <!-- #endregion Dev-only cdns, disable in production -->
+
+</body>
+
+</html>
